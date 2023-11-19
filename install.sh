d6108903d14c08ef0d31cf7b1288afecbc0d6c29
plusleft <107749872+0xleft@users.noreply.github.com>
2023-11-19 00:26:26 +0000

    update version

--- install.sh
+++ install.sh
@@ -8,11 +8,17 @@ fi
 
 # check if wget installed
 if ! [ -x "$(command -v wget)" ]; then
-  echo "Error: wget is not installed."
+  echo "Error: wget is not installed. Please install."
   exit 1
 fi
 
-# wget https://github.com/0xleft/violence/releases/download/v1.0.7/violence_linux
-wget https://github.com/0xleft/violence/releases/download/v1.0.7/violence_linux -O /usr/bin/violence
+# check if gcc installed
+if ! [ -x "$(command -v gcc)" ]; then
+  echo "Error: gcc is not installed. Please install."
+  exit 1
+fi
+
+# wget https://github.com/0xleft/violence/releases/download/v1.0.8/violence_linux
+wget https://github.com/0xleft/violence/releases/download/v1.0.8/violence_linux -O /usr/bin/violence
 chmod +x /usr/bin/violence
 echo "Violence installed in /usr/bin/violence"
