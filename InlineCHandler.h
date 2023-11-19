632be421f7ab9884d55ed8b6bc3c745a161081c6
plusleft <107749872+0xleft@users.noreply.github.com>
2023-11-19 11:56:27 +0000

    lets go

--- InlineCHandler.h
+++ InlineCHandler.h
@@ -16,6 +16,27 @@ class InlineCHandler {
 private:
     vector<string> functions;
 
+    // function to call the incline C function with args...
+    template <typename... Args>
+    std::string callFunction(void* func, Args&&... args) {
+        typedef char* (*func_t)(Args...);
+        func_t func_ptr = (func_t)func;
+        char* output = func_ptr(args...);
+        string output_string = string(output);
+        return output_string;
+    }
+
+    // helper function to transform vector<string> to Args...
+    std::string callFunctionWithVector(void* func, const vector<string>& args) {
+        std::vector<std::string> transformedArgs;
+        transformedArgs.reserve(args.size());
+        for (const auto& arg : args) {
+            transformedArgs.emplace_back(arg);
+        }
+
+        return callFunction(func, transformedArgs);
+    }
+
 public:
     string run(string filename, string function_name, vector<string> args) {
         // load the library
@@ -34,11 +55,8 @@ public:
             return "";
         }
 
-        // it is a char* return type no args function
-        typedef char* (*func_t)();
-        func_t func_ptr = (func_t)func;
-        char* output = func_ptr();
-        string output_string = string(output);
+        string output_string = callFunctionWithVector(func, args);
+
         dlclose(handle);
 
         return output_string;
